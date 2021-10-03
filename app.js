ce229dcef75d2f8d7870b9ef8680ea8963b7300f
Developer0007 <ajamadar1590@gmail.com>
2021-10-03 16:08:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ document.getElementById('Calculate').addEventListener('click',function(){
     $("#L2").val(result);
 });
 
+document.getElementById('VCalculate').addEventListener('click',function(){
+    var volratio1 = $("#VR1").val();
+    var volratio2 = $("#VR2").val();
+    var sum = $("#V").val() / (parseInt(volratio1) + parseInt(volratio2));
+
+    $("#VL1").val(sum * volratio1);
+    $("#VL2").val(sum * volratio2);
+});
